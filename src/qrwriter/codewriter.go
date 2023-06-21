1cef57979800923ce390de16381cd16bf1d0d27d
builder <builder@famillegratton.net>
2023-06-21 11:51:04 -0400

    Doc fix

--- src/qrwriter/codewriter.go
+++ src/qrwriter/codewriter.go
@@ -1,5 +1,5 @@
-// certificateManager
-// Écrit par J.F. Gratton <jean-francois@famillegratton.net>
+// qrcodewriter
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
 // Orininal name: src/qrwriter/codewriter.go
 // Original time: 2023/06/17 13:57
 
